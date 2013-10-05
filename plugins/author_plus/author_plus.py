44bf6e18e5e790f7c32b255e0933d23644b830ce
Rarylson Freitas <rarylson@vialink.com.br>
2013-10-05 01:13:07 -0300

    First functional version of Author page (using markdown)

--- plugins/author_plus/author_plus.py
+++ plugins/author_plus/author_plus.py
@@ -1,26 +1,26 @@
 import os.path
 from pelican.utils import slugify
+import markdown
 
 from logging import debug
 
 """
 Author plus plugin for Pelican
-==============================
 
 This plugin add the content of AUTHOR_PLUS_DIR/{author.url} to the ``author_plus`` variable.
 """
+
+#TODO Change the parse behaviour to Pelican readers
 
 AUTHOR_PLUS = {}
 
 def add_author_plus(generator, metadata):
 
     # Get author if it is not defined on metadata
-    #if 'author' not in metadata.keys() \
-    #    and 'AUTHOR' in generator.settings.keys():
-    #        debug("AUTHOR_PLUS: Test 0")
-    #        metadata['author'] = generator.settings['AUTHOR']
+    if 'author' not in metadata.keys() \
+        and 'AUTHOR' in generator.settings.keys():
+            metadata['author'] = generator.settings['AUTHOR']
     
-    #debug("AUTHOR_PLUS: Test 1")
     # Then add author content
     if 'author' in metadata.keys() and 'AUTHOR_PLUS_DIR' in generator.settings.keys():
         author_plus_short = slugify(metadata['author'])
@@ -31,14 +31,15 @@ def add_author_plus(generator, metadata):
             author_plus_url = "{}/{}.html".format(generator.settings['AUTHOR_PLUS_DIR'], author_plus_short)
             author_plus_url = os.path.abspath(os.path.expanduser(author_plus_url))
             debug("AUTHOR_PLUS: trying to get content from " + author_plus_url)
+            # Get content parsing the MarkDown file
             if os.path.isfile(author_plus_url):
-                # Parsing Author MD content
+                author_plus = markdown.markdownFromFile(author_plus_url)
                 with open (author_plus_url, "r") as author_file:
-                    author_plus = author_file.read().decode('utf-8')
+                    author_plus_md = author_file.read().decode('utf-8')
+                    author_plus = markdown.markdown(author_plus_md)
                 # Caching result
                 AUTHOR_PLUS.update({author_plus_short:author_plus})
                 debug(u"AUTHOR_PLUS: content about {} generated".format(author_plus_short))
             else:
                 debug("AUTHOR_PLUS: file not found " + author_plus_url)
-        
         metadata["author_plus"] = author_plus
