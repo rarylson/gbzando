cb76b5bfa0755ac01e596deba749108f9248335e
Rarylson Freitas <rarylson@vialink.com.br>
2015-09-27 19:45:25 -0300

    Ignoring a borring warning that appears in development mode
    Hide the "Feeds generated without SITEURL set properly may not be valid"
    warning.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,8 +6,8 @@ from __future__ import unicode_literals
 SITENAME = u'GBzando'
 SITESUBTITLE = u'Programação, infraestrutura e redes na prática'
 SITE_DESCRIPTION = (u'Blog de programação, infraestrutura e redes na prática. '
-                    'Artigos sobre Python, C, Shell Script (Bash), Linux, servidores web, '
-                    'email, redes, roteamento e muito mais.')
+        'Artigos sobre Python, C, Shell Script (Bash), Linux, servidores web, email, redes, '
+        'roteamento e muito mais.')
 SITE_KEYWORDS = u'programação, computação, servidores, linux, python, shell, bash, redes, gbzando'
 SITEURL = ''
 TIMEZONE = 'America/Sao_Paulo'
@@ -17,21 +17,24 @@ AUTHOR = u'Rarylson Freitas'
 AUTHOR_EMAIL = u'rarylson@gmail.com'
 
 # Disable feed generation when developing
+# Ignore also the borring warning that appears in development mode.
+import logging
+LOG_FILTER = [(logging.WARN, 'Feeds generated without SITEURL set properly may not be valid'), ]
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 
 # Blogroll
 LINKS = (
-            ('Vialink', 'http://www.vialink.com.br'),
-            ('Aprenda Python', 'http://docs.python.org/2/tutorial/'),
-            ('Aprenda Linux', 'http://www.linux.com/learn/tutorials'),
-        )
+    ('Vialink', 'http://www.vialink.com.br'),
+    ('Aprenda Python', 'http://docs.python.org/2/tutorial/'),
+    ('Aprenda Linux', 'http://www.linux.com/learn/tutorials'),
+)
 # Social widget
 SOCIAL = (
-            ('Facebook', 'https://facebook.com/rarylson'),
-            ('Stack Overflow', 'http://stackoverflow.com/users/2530295/rarylson-freitas'),
-            ('GitHub', 'https://github.com/rarylson'),
-         )
+    ('Facebook', 'https://facebook.com/rarylson'),
+    ('Stack Overflow', 'http://stackoverflow.com/users/2530295/rarylson-freitas'),
+    ('GitHub', 'https://github.com/rarylson'),
+)
 
 # Input / output
 PATH = 'content/'
@@ -69,8 +72,8 @@ TAG_CLOUD_SORTING = "alphabetically"
 # Markdown extensions
 # See: http://pythonhosted.org/Markdown/extensions/code_hilite.html#usage
 #      http://pythonhosted.org/Markdown/extensions/toc.html#usage
-MD_EXTENSIONS = ['codehilite(css_class=highlight, guess_lang=False)',
-        'extra', 'toc(anchorlink=True)', ]
+MD_EXTENSIONS = ['codehilite(css_class=highlight, guess_lang=False)', 'extra',
+        'toc(anchorlink=True)', ]
  
 # Vars for Notmyidea GBzando template
 CREATIVE_COMMONS = True
